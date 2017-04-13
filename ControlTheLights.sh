26ae4a44f164cf5893fc3e773aa17ae852489335
Dan Meltz <djm2@piratedan.com>
2017-04-13 15:40:42 -0400

    Can turn the door light on and off. Yay, off to a decent start.

---
+++ ControlTheLights.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+USERID=
+
+# Get info on the lights
+#curl http://192.168.1.2/api/$USERID/lights
+
+
+# Get info on the door light
+curl http://192.168.1.2/api/$USERID/lights/3
+
+STATE="false"
+#echo $STATE
+
+# test the door light
+#curl -H "Content-Type: application/json" -X PUT -d '{"on":true}' http://192.168.1.2/api/$USERID/lights/3/state
+#sleep 1
+#curl -H "Content-Type: application/json" -X PUT -d '{"on":false}' http://192.168.1.2/api/$USERID/lights/3/state
+
+
+exit
+
+{"state":{"on":false,"bri":178,"hue":627,"sat":247,"effect":"none","xy":[0.6606,0.3281],"ct":500,"alert":"none","colormode":"xy","reachable":true},"type":"Extended color light","name":"Door 1","modelid":"LCT001","manufacturername":"Philips","uniqueid":"00:17:88:01:00:b5:c4:45-0b","swversion":"5.23.1.13452"}%
